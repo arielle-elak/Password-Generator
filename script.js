cd2bbca520a831bdbbfd7d11fb6f2332babd77a0
Ari-chan <arielle.elak@gmail.com>
2022-08-17 20:52:50 -0600

    Logic for password length tested

--- script.js
+++ script.js
@@ -6,43 +6,54 @@ var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var symbolArr = ["!", "@", "#", "%", "^", "&", "*"];
 var pwLength = '';
+var pwArray = [];
+
+  // Function will run when the variable password is called
+var password = generatePassword();
+
+function generatePassword() {
+  const pw = pwArray.concat(symbolArr);
+  console.log("Test2");
+}
 
 // Write password to the #password input
 function writePassword() {
 
-  // Function will run when the variable password is called
-  var password = generatePassword();
+  var userLength = window.prompt("Password Length: \nPlease enter a number between 8 and 128.");
 
-  var generatePassword = function() {
-
+  // If user presses Cancel, close the window and end everything
+  if (!userLength) {
+      window.alert("Cancelled. Closing generator.")
+      return;
   }
 
-  let userLength = window.prompt("Please enter a number between 8 and 128 for the length of your password.");
-    if ((typeof userLength === 'number') && (userLength >= 8) && (userLength <= 128)) {
+  // If user inputs something into the prompt
+  // And it's a number between 8 and 128
+  if (userLength >= 8 && userLength <= 128) {
       pwLength = userLength;
-      alert("Your password will be " + pwLength + " in length.");
+      alert("Your password will be " + pwLength + " characters in length.");
+      generatePassword();
+
     } else if ((typeof userLength === 'number') && (userLength < 8) && (userLength > 128)) {
       alert("Number is out of range.");
       userLength = '';
       writePassword ();
+
     } else if ((isNaN(userLength)) &&
-      (typeof userLength === 'symbol' ||
-      typeof userLength === 'boolean' ||
-      typeof userLength === 'undefined' ||
-      typeof userLength === 'bigint' ||
-      typeof userLength === 'function' ||
-      typeof userLength === 'string')) {
+      (typeof userLength === "symbol" ||
+      typeof userLength === "boolean" ||
+      typeof userLength === "undefined" ||
+      typeof userLength === "bigint" ||
+      typeof userLength === "function" ||
+      typeof userLength === "string")) {
       alert("Numbers only, please.");
       writePassword ();
-    } else if ((typeof userLength === 'object') && (userLength === null)) {
-      alert("Cancelled. Closing generator.")
-      return;
-    }
+      }
 
-  // Execute the generate password function after writePassword is done
+  // Make the content found in #password the content of the passwordText variable
   var passwordText = document.querySelector("#password");
 
-  // passwordText is a string that will eventually contain the final password
+  // Make the value of passwordText equal the value of password, which also equals the result of the generatePassword function
   passwordText.value = password;
 
 }
