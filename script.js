cde8853b6e9d1dc9991c42a5d87223c5ddd791a5
Ari-chan <arielle.elak@gmail.com>
2022-08-17 22:15:34 -0600

    For loop in place, but not printing the correct array to the #generate tag

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code - have the button pay attention to the generate tag on the page
 var generateBtn = document.querySelector("#generate");
 
 var numberArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -6,17 +6,17 @@ var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var specialArr = ["!", "@", "#", "%", "^", "&", "*"];
 var pwLength = '';
-var pwArray = [];
+var possArray = [];
+// I STILL DO NOT UNDERSTAND HOW THIS EQUATES TO PASSWOR
 
-  // Function will run when the variable password is called
-var password = generatePassword();
-
-function generatePassword() {
-  console.log("Test2");
-}
 
 // Write password to the #password input
 function writePassword() {
+
+  // Make the content found in #password the content of the passwordText variable
+  var passwordText = document.querySelector("#password");
+  // Make the value of passwordText equal the value of password, which also equals the result of the generatePassword function
+  passwordText.value = password;
 
   var userLength = window.prompt("Password Length: \nPlease enter a number between 8 and 128.");
 
@@ -53,55 +53,59 @@ function writePassword() {
       writePassword ();
       }
 
-      var numbers = confirm("Numbers: \nWould you like to use numbers (0-9)?");
-      if (!numbers) {
-        alert("Numbers will not be used in your password.");
-        console.log("Numbers: " + numbers);
-      } else {
-        pwArray = pwArray.concat(numberArr);
-        alert("Numbers will be used in your password.");
-        console.log("Numbers: " + numbers);
-      }
+    var numbers = confirm("Numbers: \nWould you like to use numbers (0-9)?");
+    if (!numbers) {
+      alert("Numbers will not be used in your password.");
+      console.log("Numbers: " + numbers);
+    } else {
+      possArray = possArray.concat(numberArr);
+      alert("Numbers will be used in your password.");
+      console.log("Numbers: " + numbers);
+    }
 
-      var upperCase = confirm("UPPERCASE Characters: \nWould you like to use UPPERCASE characters (A-Z)?");
-      if (!upperCase) {
-        alert("UPPERCASE characters not be used in your password.");
-        console.log("Uppercase: " + upperCase);
-      } else {
-        pwArray = pwArray.concat(upperArr);
-        alert("UPPERCASE characters will be used in your password.");
-        console.log("Uppercase: " + upperCase);
-      }
+    var upperCase = confirm("UPPERCASE Characters: \nWould you like to use UPPERCASE characters (A-Z)?");
+    if (!upperCase) {
+      alert("UPPERCASE characters not be used in your password.");
+      console.log("Uppercase: " + upperCase);
+    } else {
+      possArray = possArray.concat(upperArr);
+      alert("UPPERCASE characters will be used in your password.");
+      console.log("Uppercase: " + upperCase);
+    }
 
-      var lowerCase = confirm("Lowercase Characters: \nWould you like to use lowercase characters (a-z)?");
-      if (!lowerCase) {
-        alert("Lowercase characters will not be used in your password.")
-        console.log("Lowercase: " + lowerCase);
-      } else {
-        pwArray = pwArray.concat(lowerArr);
-        alert("Lowercase characters will be used in your password.")
-        console.log("Lowercase: " + lowerCase);
-      }
+    var lowerCase = confirm("Lowercase Characters: \nWould you like to use lowercase characters (a-z)?");
+    if (!lowerCase) {
+      alert("Lowercase characters will not be used in your password.")
+      console.log("Lowercase: " + lowerCase);
+    } else {
+      possArray = possArray.concat(lowerArr);
+      alert("Lowercase characters will be used in your password.")
+      console.log("Lowercase: " + lowerCase);
+    }
 
-      var special = confirm("Special Characters: \nWould you like to use special characters (a-z)?");
-      if (!special) {
-        alert("Special characters will not be used in your password.")
-        console.log("Special: " + special);
-      } else {
-        pwArray = pwArray.concat(specialArr);
-        alert("Special characters will be used in your password.")
-        console.log("Special: " + special);
-      }
+    var special = confirm("Special Characters: \nWould you like to use special characters (a-z)?");
+    if (!special) {
+      alert("Special characters will not be used in your password.")
+      console.log("Special: " + special);
+    } else {
+      possArray = possArray.concat(specialArr);
+      alert("Special characters will be used in your password.")
+      console.log("Special: " + special);
+    }
 
-      console.log(pwArray);
+      console.log(possArray);
+      console.log(password);
+      passwordText = [];
 
+      for (var i=0; i < pwLength; i++) {
+        // This statement will run each time the loop is executed
+        const character = possArray[Math.floor(Math.random() * possArray.length)];
+        // Add a new character to the end of the passwordText array
 
-  // Make the content found in #password the content of the passwordText variable
-  var passwordText = document.querySelector("#password");
+        passwordText.push(character);
 
-  // Make the value of passwordText equal the value of password, which also equals the result of the generatePassword function
-  passwordText.value = password;
-
+        }
+        console.log(passwordText.value);
 }
 
 // Add event listener to generate button
