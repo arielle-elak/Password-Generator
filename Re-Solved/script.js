5c0ed43ba1fd7c0cec6587958e2395cd97ebd7fa
Ari-chan <arielle.elak@gmail.com>
2022-10-22 17:26:24 -0600

    Fully commenting each step

--- Re-Solved/script.js
+++ Re-Solved/script.js
@@ -4,14 +4,18 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
+    // Select the area denoted by #password as save it as the passwordText variable
     var passwordText = document.querySelector("#password");
 
+    // The value of password will be equal to what generatePassword returns
     var password = generatePassword();
 
-    function generatePassword() {
+     // generatePassword will ask the questions to determine the content of the password
+     function generatePassword() {
         return "Test"
     };
 
+    // Make the HTML text value inside of #passoword equal to whatever the password variable equa;s
     passwordText.value = password;
 
 }
