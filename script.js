a0ec73b76ab89c85d120d6a3a5a3fac08866caff
Ari-chan <arielle.elak@gmail.com>
2022-10-22 20:42:50 -0600

    Generator seems to work with intended output

--- script.js
+++ script.js
@@ -1,198 +1,232 @@
-// Password Generator
-// Last updated by Arielle Schlickman Elak Aug 18 2022
+// Password Generator .js Re-vamped
+// Last Edited 10-22-2022
 
-/* ----------------------
-//MAIN ISSUE
-If the window captures anything before the initial cancel is hit, it will skip all possibilities and go on to the next phase without retrieving a length
-*/
+// Array from which random characters will be selected
+var generatorArr = [];
+
+// Array for final generated password
+var passwordArr = [];
+
+var specialCharacters = [
+    '!',
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*'
+];
+
+var numericCharacters = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+];
+
+var upperCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+];
+
+var lowerCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+];
+
+
+var passwordObject = {
+    "pwlength": 0,
+    "special": false,
+    "numeric": false,
+    "upper": false,
+    "lower": false,
+}
 
 
 // Assignment Code
-//The id of the button in HTML is "generate"
-// The listener at the bottom will watch for activity in the button#generate zone
 var generateBtn = document.querySelector("#generate");
 
-// Global variables that will change depending upon the user's input
-var pwLength = 0;
-var numbers = true;
-var upper = true;
-var lower = true;
-var special = true;
+function promptPassword() {
 
-// All possible characters in arrays
-var arrSpecial = ["!","@","#","$","%","^","?"],
-arrNumbers = [0,1,2,3,4,5,6,7,8,9],
-arrUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-arrLowercase = ["a","b","b","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    // Make sure output gets turned into an integer
+    let userLength = prompt(
+        "Password Length:\nPlease enter a number between 8 and 128.");
 
-// This will be the array container for the specified range of characters based on true/false statements to be randomly selected from
-var generatorCharacters = []
+    // If user types exit and OK then the wizard will close
+    if (userLength === "exit" || userLength === "c" || userLength === "C" || userLength === "EXIT" || userLength === "Exit") {
+        console.log(userLength);
+        alert("Wizard will exit.");
+        return;
 
-// The array in which the final password characters will be stored before being turned into a string
-var arrPassword = [];
+    // If valid, log the value to the passwordObject and start asking about options
+    }  else if ((userLength >= 8) && (userLength <= 128)) {
+        alert("Your password will be " + userLength + " characters in length.");
+        passwordObject.pwlength = userLength;
+        console.log(passwordObject);
+        characterOptions();
 
-// When the button is clicked, it will call the funciton writePassword
+    } else if (userLength < 8 || userLength > 128) {
+        alert("Password must be at least 8 but no more than 128 characters in length.")
+        userLength = "";
+        console.log("Length issue: " + userLength);
+        promptPassword();
+
+    } else if (isNaN(userLength) || userLength === "") {
+        console.log(userLength);
+        alert("Please enter a valid number or type 'Exit' then press OK to close the generator.");
+        userLength = "";
+        console.log("Invalid: " + userLength);
+        promptPassword();
+    };
+};
+
+
+
+    // Confirm on alert to select type of characters for password
+    // If user does not hit OK on any options, wizard will start over
+
+function characterOptions() {
+
+    var userSpecial = confirm(
+            'Would you like to include special characters?\nClick OK for "Yes" or Cancel for "No".'
+        );
+
+        var userNumeric = confirm(
+            'Would you like to include numbers?\nClick OK for "Yes" or Cancel for "No".'
+        );
+
+        var userUpper = confirm(
+            'Would you like to include UPPERCASE letters?\nClick OK for "Yes" or Cancel for "No".'
+        );
+
+        var userLower = confirm(
+            'Would you like to include lowercase letters?\nClick OK for "Yes" or Cancel for "No".'
+        );
+
+        // If no characters selected, return to start of character options
+        if (userSpecial === false
+            && userNumeric === false
+            && userUpper === false
+            && userLower === false) {
+                alert("Password must have at least one character type.");
+                characterOptions();
+        };
+
+        // If selection is true, concat the appropriate array into the generator array
+        if (userSpecial === true) {
+            passwordObject.special = true;
+            console.log("Special characters selected")
+            generatorArr = generatorArr.concat(specialCharacters);
+        };
+
+        if (userNumeric === true) {
+            passwordObject.numeric = true;
+            console.log("Numeric characters selected")
+            generatorArr = generatorArr.concat(numericCharacters);
+        };
+
+        if (userUpper === true) {
+            passwordObject.upper = true;
+            console.log("Uppercase characters selected")
+            generatorArr = generatorArr.concat(upperCharacters);
+        };
+
+        if (userLower === true) {
+            passwordObject.lower = true;
+            console.log("Lowercase characters selected")
+            generatorArr = generatorArr.concat(lowerCharacters);
+        };
+    console.log("Selections made");
+    console.log("Characters to use in generation: " + generatorArr);
+};
+
+// Generate the password based on the user prompts
+function generatePassword() {
+    promptPassword();
+
+    for (var i = 0; i < passwordObject.pwlength; i++) {
+
+        // This statement will run each time the loop is executed
+        const character = generatorArr[Math.floor(Math.random() * generatorArr.length)];
+        // Add a new character to the end of the arrPassword array
+        passwordArr.push(character);
+    }
+
+    // Combine the elements of the array into a string
+    let password = passwordArr.join("");
+    return password;
+};
+
+// Write outcome of generatePassword as the password variable
+// Sent that content to the #password HTML section on the page
 function writePassword() {
-  // Reset any old data when button is pressed again.
-  // Clears for the next round
-  generatorCharacters = [];
-  arrPassword = [];
-  passwordText = [];
-  pwLength = '';
-  userLength = false;
-
-   // This will watch for when the var passwordText has a value assigned
-  // And to link it to the area on the page that has the password id
-  var passwordText = document.querySelector("#password");
-
-// Data collected from prompt window
-  var userLength = window.prompt("Password Length: \nPlease enter a number between 8 and 128.");
-  // If user presses Cancel, close the window and end everything
-  debugger
-
-  // IS it type? variable value?
-  /**
-   *
-   *
-   * 
-   */
-  if (!userLength) {
-    window.alert("Cancelled. Closing generator.");
-    userLength = null;
-    console.log("Cancelled " + userLength);
-    return;
-
-  } else if (userLength >= 8 && userLength <= 128) {
-    pwLength = userLength;
-    alert("Your password will be " + pwLength + " characters in length.");
-    console.log("Password length: " + pwLength);
-
-// If it's not between 8 and 128
-  } else if ((userLength < 8 || userLength > 128)) {
-    alert("Number is out of range.");
-    console.log("Password length: Number of range");
-    userLength = null;
-    writePassword();
-
-// If it's not a number and one of these other types (except for object since we want null to be possible for the cancel)
-  } else if ((isNaN(userLength)) ||
-    typeof userLength === "symbol" ||
-    typeof userLength === "boolean" ||
-    typeof userLength === "undefined" ||
-    typeof userLength === "bigint" ||
-    typeof userLength === "function" ||
-    typeof userLength === "string") {
-    alert("Numbers only, please.");
-    console.log("Password length: Invalid characters");
-    console.log(userLength + " for the isNan");
-    writePassword();
-
-  } else {
-    console.log("Something else happened...")
-    userLength = null;
-    return;
-  }
-
-  var numbers = confirm("Numbers: \nWould you like to use numbers (0-9)?");
-  if (!numbers) {
-    alert("Numbers will not be used in your password.");
-    console.log("Numbers: " + numbers);
-  } else {
-    generatorCharacters = generatorCharacters.concat(arrNumbers);
-    alert("Numbers will be used in your password.");
-    console.log("Numbers: " + numbers);
-  }
-
-  var upperCase = confirm("UPPERCASE Characters: \nWould you like to use UPPERCASE characters (A-Z)?");
-  if (!upperCase) {
-    alert("UPPERCASE characters not be used in your password.");
-    console.log("Uppercase: " + upperCase);
-  } else {
-    generatorCharacters = generatorCharacters.concat(arrUppercase);
-    alert("UPPERCASE characters will be used in your password.");
-    console.log("Uppercase: " + upperCase);
-  }
-
-  var lowerCase = confirm("Lowercase Characters: \nWould you like to use lowercase characters (a-z)?");
-  if (!lowerCase) {
-    alert("Lowercase characters will not be used in your password.")
-    console.log("Lowercase: " + lowerCase);
-  } else {
-    generatorCharacters = generatorCharacters.concat(arrLowercase);
-    alert("Lowercase characters will be used in your password.")
-    console.log("Lowercase: " + lowerCase);
-  }
-
-  var special = confirm("Special Characters: \nWould you like to use special characters (a-z)?");
-  if (!special) {
-    alert("Special characters will not be used in your password.")
-    console.log("Special: " + special);
-  } else {
-    generatorCharacters = generatorCharacters.concat(arrSpecial);
-    alert("Special characters will be used in your password.")
-    console.log("Special: " + special);
-  }
-
-
-
-  // Generate the actual password!!!!
-  var password = generatePassword();
-  function generatePassword () {
-  for(var i=0; i < pwLength; i++) {
-    // This statement will run each time the loop is executed
-    const character = generatorCharacters[Math.floor(Math.random() * generatorCharacters.length)];
-    // Add a new character to the end of the arrPassword array
-    arrPassword.push(character);
-  }
-  // Combine the elements of the array into a string
-  passwordText = arrPassword.join("");
-  // This will be the password the user sees
-  alert("Your password will be in the box below.");
-  console.log(passwordText);
-  //Make the password appear on screen in the #password tagged element on the page!
-  document.querySelector("#password").innerHTML = passwordText;
-  console.log(generatorCharacters);
-
-
-  // Clears for the next round
-  generatorCharacters = [];
-  console.log(generatorCharacters);
-  arrPassword = [];
-  console.log(arrPassword);
-  passwordText = [];
-  console.log(passwordText);
-  userLength = false;
-  console.log(userLength);
-  }
+    // Clear the arrays for next use if previously contained values
+    passwordArr = [];
+    generatorArr = [];
+    // Set the password variable to equal the output from the generatePassword function
+    var password = generatePassword();
+    // Select the content in the #password tag on the HTML page
+    var passwordText = document.querySelector("#password");
+    // Set the content of the #password section to the password content
+    passwordText.value = password;
 }
+
 // Add event listener to generate button
-// This listener once assigned to the id to pay attention to, will start the writePassword function
-// DO NOT MOVE FROM THE END OF THIS CODE
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
- /* function newWritePassword() {
-
-  var userLength = window.prompt("Password Length: \nPlease enter a number between 8 and 128.");
-
-  while (userLength&&((isNaN(userLength) || userLength < 8 || userLength > 128) {
-    alert("Please enter a valid number between 8 and 128.");
-    window.prompt("Password Length: \nPlease enter a number between 8 and 128.");
-  }
-
-  if (!userLength) {
-    window.alert("Cancelled. Closing generator.");
-    userLength = null;
-    console.log("Cancelled " + userLength);
-    return;
-
-  } else if (userLength >= 8 && userLength <= 128) {
-    pwLength = userLength;
-    alert("Your password will be " + pwLength + " characters in length.");
-    console.log("Password length: " + pwLength);
-  }
-}
-
-*/
